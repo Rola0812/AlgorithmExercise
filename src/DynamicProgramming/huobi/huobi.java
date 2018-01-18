1b90043caa9b0b2e3535c4967cf4ab0b1808ea3d
Rola0812 <445055925@qq.com>
2018-01-18 20:07:06 +0800

    算法练习

--- src/DynamicProgramming/huobi/huobi.java
+++ src/DynamicProgramming/huobi/huobi.java
@@ -7,9 +7,9 @@ public class huobi {
         int[] arr = {5,10,25,1};
         int aim = 1000;
 
-        int c[][] = huobi_Solution(arr,aim);
+        int c[][] = huobi_Solution(arr,0,aim);
 
-        for(int i = 0;i<=4;i++){
+        for(int i = 0;i<4;i++){
             for(int j = 0;j<=aim;j++){
                 System.out.print(c[i][j]+"\t");
                 if(j==aim){
@@ -20,29 +20,35 @@ public class huobi {
 
     }
 
-    private static int[][] huobi_Solution(int[] arr,int aim){
+    private static int[][] huobi_Solution(int[] arr,int index,int aim){
 
-        if(arr == null || aim<0){
+        if(arr == null || aim<0 ||arr.length == 0){
             return null;
         }
         //dp[n][m]为使用前n中货币凑成的m的种数
-        int dp[][] = new int[5][aim+1];
+        int dp[][] = new int[arr.length][aim+1];
 
-        for(int i = 0;i<5;i++){
+        for(int i = 0;i<arr.length;i++){
             dp[i][0]=1;
         }
         for(int i = 1;arr[0]*i<=aim;i++){
             dp[0][arr[0]*i] = 1;
         }
 
-//        for(int i = 0;i<5;i++){
-//            dp[i][0] = 1;
-//        }
-//        for(int j = 0;j<aim+1;j++){
-//            dp[0][j] = 1;
-//        }
 
-        for(int i = 1;i<5;i++){
+        for(int i = 1;i<arr.length;i++){
+            for(int j = 1;j<=aim;j++){
+                int num = 0;
+                for(int k = 0;j-k*arr[i]>=0;k++){
+                    num += dp[i-1][j-k*arr[i]];
+                }
+                dp[i][j] = num;
+            }
+        }
+        return dp;
+
+
+       /* for(int i = 1;i<5;i++){
             for(int j = 0;j<aim+1;j++){
                 if(j>=arr[i-1]){//j比i的面值大
                     //System.out.println("i="+i+",j="+j);
@@ -54,6 +60,6 @@ public class huobi {
                 }
             }
         }
-        return dp;
+        return dp;*/
     }
 }
